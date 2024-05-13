12464938f3e69200e98600594ad20afaf1ad13a7
os3k1mtr1l <rud.vlad2015@hotmail.com>
2024-05-13 02:05:36 +0200

    Added apple setup

--- src/setup.cpp
+++ src/setup.cpp
@@ -5,4 +5,10 @@ void setup(Game& snake) //main setup function
 {
     snake.score = 0;
     snake.player = Player(CENTER_X, CENTER_Y, 10, 10);
+    snake.player.speed = 25;
+    snake.player.direction = 1;
+    snake.player.horizontal = true;
+
+    snake.apple = {0,0,10,10};
+    snake.newApple();
 }
